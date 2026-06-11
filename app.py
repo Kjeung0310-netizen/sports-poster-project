5145c04d7ded076618015a9dac7752eef6bb01e0
Kjeung0310-netizen <Kjeung0310@icloud.com>
2026-06-12 01:18:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("⚾ 나만의 응원 포스터 제작기")
 
 # 1. 왼쪽 설정창
 with st.sidebar:
-    user_text = st.text_input("응원 문구 입력:", "제발 네모안에 공을 던져")
+    user_text = st.text_input("응원 문구 입력:", "네모안에 공을 던져")
     size_option = st.select_slider("글자 크기", options=["작게", "중간", "크게"], value="중간")
     logo_option = st.selectbox("로고 선택", ["로고 없음", "KIA", "KT", "LG", "Samsung"])
 
