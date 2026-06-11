0a5bfa8009e38d8be66ee9cd5dda0de879e0a391
Kjeung0310-netizen <Kjeung0310@icloud.com>
2026-06-12 01:19:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,42 +3,36 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title("⚾ 나만의 응원 포스터 제작기")
 
-# 1. 왼쪽 설정창
+# 1. 사이드바
 with st.sidebar:
     user_text = st.text_input("응원 문구 입력:", "네모안에 공을 던져")
     size_option = st.select_slider("글자 크기", options=["작게", "중간", "크게"], value="중간")
     logo_option = st.selectbox("로고 선택", ["로고 없음", "KIA", "KT", "LG", "Samsung"])
 
-# 2. 메인 화면
-col1, col2 = st.columns([1, 3])
+# 2. 포스터 영역 (컨테이너 사용)
+st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
 
-with col2:
-    # 로고를 사진 위에 띄우는 스타일
-    logo_html = ""
-    if logo_option != "로고 없음":
-        logo_html = f'<img src="https://raw.githubusercontent.com/Kjeung0310-netizen/poster-project/main/{logo_option}.png" style="width:150px; position:absolute; top:20px; left:20px;">'
+# 배경 이미지
+st.image("baseball.jpg", use_container_width=True)
 
-    # 배경 이미지 출력
-    st.markdown(f"""
-        <div style="position: relative;">
-            <img src="https://raw.githubusercontent.com/Kjeung0310-netizen/poster-project/main/baseball.jpg" style="width:100%; border-radius:10px;">
-            {logo_html}
-        </div>
-    """, unsafe_allow_html=True)
-    
-    # 노란 글씨 + 검은 테두리 스타일
-    font_map = {"작게": "50px", "중간": "90px", "크게": "130px"}
-    
-    st.markdown(f"""
-        <div style="
-            text-align: center; 
-            margin-top: -300px;
-            font-size: {font_map[size_option]}; 
-            font-weight: bold; 
-            color: #FFFF00; 
-            -webkit-text-stroke: 3px black; 
-            text-shadow: 4px 4px 0px black;
-        ">
-            {user_text}
-        </div>
-    """, unsafe_allow_html=True)
+# 로고 출력 (공식 명령어 사용)
+if logo_option != "로고 없음":
+    st.image(f"{logo_option}.png", width=150)
+
+# 텍스트 출력
+font_map = {"작게": "50px", "중간": "90px", "크게": "130px"}
+st.markdown(f"""
+    <div style="
+        margin-top: -300px;
+        text-align: center; 
+        font-size: {font_map[size_option]}; 
+        font-weight: bold; 
+        color: #FFFF00; 
+        -webkit-text-stroke: 3px black; 
+        text-shadow: 4px 4px 0px black;
+    ">
+        {user_text}
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("</div>", unsafe_allow_html=True)
