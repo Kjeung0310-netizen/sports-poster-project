89427dc8d440fa19f393fd46ab6514f54b70c364
Kjeung0310-netizen <Kjeung0310@icloud.com>
2026-06-12 01:28:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 
 st.set_page_config(layout="wide")
 st.title("⚾ 나만의 응원 포스터 제작기")
@@ -8,31 +9,34 @@ with st.sidebar:
     size_option = st.select_slider("글자 크기", options=["작게", "중간", "크게"], value="중간")
     logo_option = st.selectbox("로고 선택", ["로고 없음", "KIA", "KT", "LG", "Samsung"])
 
-# 글자 크기 매핑
-font_map = {"작게": "40px", "중간": "80px", "크게": "120px"}
+# 파일 로드 함수 (깃허브 서버 내 파일 경로)
+def get_image_base64(path):
+    with open(path, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-# 로고 HTML 생성
+# 베이스 64로 변환하여 로딩 (이미지 엑박 방지)
+bg_img = get_image_base64("baseball.jpg")
 logo_html = ""
 if logo_option != "로고 없음":
-    logo_html = f'<img src="https://raw.githubusercontent.com/Kjeung0310-netizen/poster-project/main/{logo_option}.png" style="width: 120px; position: absolute; top: 10px; left: 10px;">'
+    logo_img = get_image_base64(f"{logo_option}.png")
+    logo_html = f'<img src="data:image/png;base64,{logo_img}" style="width: 150px; position: absolute; top: 10px; left: 10px;">'
 
-# 핵심: 사진, 로고, 글자를 하나의 박스(relative)에 넣어서 절대 안 밀리게 함!
+font_map = {"작게": "40px", "중간": "80px", "크게": "120px"}
+
+# 최종 렌더링
 st.markdown(f"""
     <div style="position: relative; width: 100%;">
-        <img src="https://raw.githubusercontent.com/Kjeung0310-netizen/poster-project/main/baseball.jpg" style="width: 100%; border-radius: 10px;">
+        <img src="data:image/jpeg;base64,{bg_img}" style="width: 100%; border-radius: 10px;">
         {logo_html}
         <div style="
             position: absolute; 
-            top: 50%; 
-            left: 50%; 
-            transform: translate(-50%, -50%);
-            width: 100%;
-            text-align: center; 
+            top: 50%; left: 50%; 
+            transform: translate(-50%, -50%); 
+            width: 100%; text-align: center; 
             font-size: {font_map[size_option]}; 
-            font-weight: bold; 
-            color: #FFFF00; 
-            -webkit-text-stroke: 2px black; 
-            text-shadow: 4px 4px 0px black;
+            font-weight: bold; color: #FFFF00; 
+            -webkit-text-stroke: 2px black; text-shadow: 4px 4px 0px black;
         ">
             {user_text}
         </div>
