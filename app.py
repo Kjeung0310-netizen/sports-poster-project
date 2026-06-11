3d31412c5e43873689796806be197781c0233de3
Kjeung0310-netizen <Kjeung0310@icloud.com>
2026-06-12 00:58:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 st.title("⚾ 나만의 응원 포스터 제작기")
 
 with st.sidebar:
-    user_text = st.text_input("응원 문구:", "홈런을 쳐")
+    user_text = st.text_input("응원 문구:", "네모 안에 공을 던져")
     size_option = st.select_slider("글자 크기", options=["작게", "중간", "크게"], value="중간")
     logo_option = st.selectbox("로고 선택", ["로고 없음", "KIA", "KT", "LG", "Samsung"])
 
